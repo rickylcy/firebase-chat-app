e5aeab7cfbc6f2dea5cc07205bacf59ba9046979
Ricky Lau <lauyin8183@gmail.com>
2024-08-28 15:39:40 +1000

    renaming

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -18,7 +18,7 @@ export default function Navbar({ user }) {
       }}
     >
       <Typography level="h4" sx={{ fontSize: 24 }}>
-        WANG BO QI
+        Chat Room
       </Typography>
       <Stack direction="row" spacing={2} alignItems="center">
         <ColorSchemeToggle />
